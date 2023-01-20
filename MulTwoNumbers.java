6aec9b875de178339881ff2073dcd8af9cb83d89
Pallavi-work <103096787+Pallavi-work@users.noreply.github.com>
2023-01-20 16:21:26 +0530

    Add files via upload
    (cherry picked from commit 7435dcc515cd0e82311c209e1f59b87474bffac2)

---
+++ MulTwoNumbers.java
@@ -0,0 +1,12 @@
+public class MulTwoNumbers
+{
+	public static void main(String args[])
+	{
+		int firstno = 5;
+		int secondno = 6;
+		System.out.println("First no is"+firstno);
+		System.out.println("Second no is"+secondno);
+		int prod = firstno + secondno;
+		System.out.println("Product is:"+prod);
+	}
+}
